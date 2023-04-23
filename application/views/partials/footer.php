e2473845b277a027c330e379bfbd5830343ac263
Ur github Username <manurunghutagurgur168@gmail.com>
2023-04-23 19:56:35 +0700

    first commit

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <?php echo date("Y"); ?> <a href="https://instagram.com/" target="_blank">Eben Manurung</a></span>
+		</div>
+	</div>
+</footer>
